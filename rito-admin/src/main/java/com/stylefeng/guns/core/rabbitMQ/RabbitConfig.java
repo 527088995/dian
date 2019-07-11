d7aa483eee68917869d65c04b0ee17573aa8bd63
527088995 <wu2612103>
2019-07-11 14:24:48 +0800

    以mq方式存储notice

--- rito-admin/src/main/java/com/stylefeng/guns/core/rabbitMQ/RabbitConfig.java
+++ rito-admin/src/main/java/com/stylefeng/guns/core/rabbitMQ/RabbitConfig.java
@@ -4,13 +4,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class RabbitConfig {
@@ -34,28 +29,30 @@ public class RabbitConfig {
      * 针对消费者配置
      * 1. 设置交换机类型
      * 2. 将队列绑定到交换机
-     FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
-     HeadersExchange ：通过添加属性key-value匹配
-     DirectExchange:按照routingkey分发到指定队列
-     TopicExchange:多关键字匹配
+     * FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
+     * HeadersExchange ：通过添加属性key-value匹配
+     * DirectExchange:按照routingkey分发到指定队列
+     * TopicExchange:多关键字匹配
      */
     @Bean
     public DirectExchange defaultExchange() {
         return new DirectExchange(EXCHANGE_A);
     }
+
     /**
      * 获取队列A
+     *
      * @return
      */
     @Bean
     public Queue queueA() {
         return new Queue(QUEUE_A, true); //队列持久
     }
+
     @Bean
     public Binding binding() {
         return BindingBuilder.bind(queueA()).to(defaultExchange()).with(RabbitConfig.ROUTINGKEY_A);
     }
-
 
 
 }
