b9fa678752bdd926341eb429db8783040fb703ca
527088995 <527088995@qq.com>
2019-10-12 10:13:30 +0800

    集成单点登陆

--- rito-admin/src/test/java/com/stylefeng/guns/flowable/FlowableDbInitTest.java
+++ rito-admin/src/test/java/com/stylefeng/guns/flowable/FlowableDbInitTest.java
@@ -13,15 +13,40 @@ import org.junit.Test;
  */
 public class FlowableDbInitTest {
 
-    @Test
-    public void init() {
-        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
-                .setJdbcUrl("jdbc:mysql://127.0.0.1:3306/flowable?autoReconnect=true&useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull")
-                .setJdbcUsername("root")
-                .setJdbcPassword("root")
-                .setJdbcDriver("com.mysql.jdbc.Driver")
-                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+//    @Test
+//    public void init() {
+//        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+//                .setJdbcUrl("jdbc:mysql://106.13.55.232:3306/guns?autoReconnect=true&useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=CONVERT_TO_NULL&useSSL=false&serverTimezone=CTT")
+//                .setJdbcUsername("root")
+//                .setJdbcPassword("1qaz!QAZ")
+//                .setJdbcDriver("com.mysql.cj.jdbc.Driver")
+//                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+//
+//        ProcessEngine processEngine = cfg.buildProcessEngine();
+//    }
 
-        ProcessEngine processEngine = cfg.buildProcessEngine();
+    /**使用代码创建工作流需要的23张表*/
+    @Test
+    public void createTable() {
+        ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration
+                .createStandaloneProcessEngineConfiguration();
+        //连接数据库的配置
+        //配置数据库驱动:对应不同数据库类型的驱动
+        processEngineConfiguration.setJdbcDriver("com.mysql.cj.jdbc.Driver");
+        //配置数据库的JDBC URL
+        processEngineConfiguration.setJdbcUrl("jdbc:mysql://106.13.55.232:3306/guns?autoReconnect=true&useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=CONVERT_TO_NULL&useSSL=false&serverTimezone=CTT&nullCatalogMeansCurrent=true");
+        //配置连接数据库的用户名
+        processEngineConfiguration.setJdbcUsername("root");
+        //配置连接数据库的密码
+        processEngineConfiguration.setJdbcPassword("1qaz!QAZ");
+        /**
+         public static final String DB_SCHEMA_UPDATE_FALSE = "false";不能自动创建表，需要表存在
+         public static final String DB_SCHEMA_UPDATE_CREATE_DROP = "create-drop";先删除表再创建表
+         public static final String DB_SCHEMA_UPDATE_TRUE = "true";如果表不存在，自动创建表
+         */
+        processEngineConfiguration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        //工作流的核心对象，ProcessEnginee对象
+        ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+        System.out.println("processEngine:"+processEngine);
     }
 }
