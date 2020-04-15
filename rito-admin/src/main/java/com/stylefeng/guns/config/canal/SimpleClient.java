91e216ce9c5fda794bce1738bd08d3de668e89df
527088995 <527088995@qq.com>
2020-04-15 17:07:24 +0800

    canal抓取mysql数据 mysql版本要低我使用的是5.7

---
+++ rito-admin/src/main/java/com/stylefeng/guns/config/canal/SimpleClient.java
@@ -0,0 +1,81 @@
+package com.stylefeng.guns.config.canal;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.protocol.CanalEntry.Column;
+import com.alibaba.otter.canal.protocol.CanalEntry.Entry;
+import com.alibaba.otter.canal.protocol.CanalEntry.EntryType;
+import com.alibaba.otter.canal.protocol.CanalEntry.RowChange;
+import com.alibaba.otter.canal.protocol.CanalEntry.RowData;
+import com.alibaba.otter.canal.protocol.Message;
+
+import static com.stylefeng.guns.core.support.DateTimeKit.now;
+
+public class SimpleClient {
+
+    public static void main(String[] args) throws Exception {
+        CanalConnector connector = CanalConnectors.newSingleConnector(
+                new InetSocketAddress("106.13.37.72", 11111), "example", "", "");
+
+        connector.connect();
+        connector.subscribe(".*\\..*");
+        connector.rollback();
+
+        while (true) {
+            Message message = connector.getWithoutAck(100);
+            long batchId = message.getId();
+            if (batchId == -1 || message.getEntries().isEmpty()) {
+                System.out.println("sleep"+now());
+                Thread.sleep(1000);
+                continue;
+            }
+            printEntries(message.getEntries());
+            connector.ack(batchId);
+        }
+    }
+
+    private static void printEntries(List<Entry> entries) throws Exception {
+        for (Entry entry : entries) {
+            if (entry.getEntryType() != EntryType.ROWDATA) {
+                continue;
+            }
+
+            RowChange rowChange = RowChange.parseFrom(entry.getStoreValue());
+            for (RowData rowData : rowChange.getRowDatasList()) {
+                switch (rowChange.getEventType()) {
+                    case INSERT:
+                    case UPDATE:
+                        System.out.print("UPSERT ");
+                        printColumns(rowData.getAfterColumnsList());
+
+                        if ("retl_buffer".equals(entry.getHeader().getTableName())) {
+                            String tableName = rowData.getAfterColumns(1).getValue();
+                            String pkValue = rowData.getAfterColumns(2).getValue();
+                            System.out.println("SELECT * FROM " + tableName + " WHERE id = " + pkValue);
+                        }
+                        break;
+
+                    case DELETE:
+                        System.out.print("DELETE ");
+                        printColumns(rowData.getBeforeColumnsList());
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private static void printColumns(List<Column> columns) {
+        String line = columns.stream()
+                .map(column -> column.getName() + "=" + column.getValue())
+                .collect(Collectors.joining(","));
+        System.out.println(line);
+    }
+
+}
